68ec70872ff331f65ed7f1b450e5fd79e4a5628c
buz321 <107760647+buz321@users.noreply.github.com>
2023-04-04 04:32:58 +0900

    Update 최댓값 만들기 （1）.py

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,5 +1,5 @@
 def solution(numbers):
-    numbers.sort(reverse= True)
-    answer = numbers[0] * numbers [1]
+    numbers.sort(reverse= True) # 큰수가 맨 앞으로 오게 정렬(sort)
+    answer = numbers[0] * numbers [1] # 리스트 값을 설정 하여 계산
         
     return answer
