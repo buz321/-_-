d7dfec228abab2375b4119563c685c669ab07ab4
buz321 <107760647+buz321@users.noreply.github.com>
2023-04-05 05:20:15 +0900

    Update 아이스 아메리카노.py

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,7 +1,7 @@
 def solution(money):
     answer = []
     if money % 5500 == 0:
-        answer = [money // 5500, 0]
+        answer = [money // 5500, 0] # 튜
     else:
         answer = [money // 5500 , money - (5500*(money // 5500))]
     return answer
