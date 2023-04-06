7f22fa0d93af33d77b33e9940baacda916b1fa7c
buz321 <107760647+buz321@users.noreply.github.com>
2023-04-07 04:24:47 +0900

    Update 짝수는 싫어요.py

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = []
-    for i in range(0, n+1):
-        if i%2== 1:
+    for i in range(0, n+1): # 범위 설정
+        if i%2== 1: # 홀수값
             answer.append(i)
     return answer
